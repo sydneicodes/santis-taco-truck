a0e25eea54490a8c70e631c41d4de33b3987934f
Sydnei Davis <sydnei.dev@gmail.com>
2022-11-06 04:26:52 -0500

    first commit: need to fix output and css

---
+++ app/routes.js
@@ -0,0 +1,128 @@
+const { ObjectID } = require("mongodb");
+
+module.exports = function (app, passport, db) {
+
+  // normal routes ===============================================================
+
+  // show the home page (will also have our login links)
+  app.get('/', function (req, res) {
+    res.render('index.ejs');
+  });
+
+  // PROFILE SECTION =========================
+  app.get('/profile', isLoggedIn, function (req, res) {
+    db.collection('orders').find().toArray((err, result) => {
+      if (err) return console.log(err)
+      console.log(result)
+      res.render('profile.ejs', {
+        user: req.user,
+        orders: result
+      })
+    })
+  });
+
+  // LOGOUT ==============================
+  app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/');
+  });
+
+  // message board routes ===============================================================
+
+  app.post('/taco_order', (req, res) => {
+    // Filter through req.body and only return the properties that don't have an empty string value
+    let arr = Object.entries(req.body)
+    const filtered = arr.filter(([key, value]) => {
+      return value !== ''
+    });
+    const order = Object.fromEntries(filtered);
+    order.time = new Date().toLocaleTimeString()
+    order.completed = false 
+
+    db.collection('orders').save({ order: order}, (err, result) => {
+      if (err) return console.log(err)
+      console.log('saved to database')
+      res.redirect('/profile')
+    })
+  })
+
+  app.put('/completed_orders', (req, res) => {
+    console.log(req.body)
+    db.collection('orders')
+      .findOneAndUpdate({ id: req.body.id }, {
+        $set: {
+          completed: true
+        }
+      }, {
+        sort: { _id: -1 },
+        upsert: true
+      }, (err, result) => {
+        if (err) return res.send(err)
+        res.send(result)
+      })
+  })
+  
+  app.delete('/orders', (req, res) => {
+    db.collection('orders').findOneAndDelete({_id : ObjectId(req.body._id) }, (err, result) => {
+      if (err) return res.send(500, err)
+      res.send('Order deleted!')
+    })
+  })
+
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
+
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get('/login', function (req, res) {
+    res.render('login.ejs', { message: req.flash('loginMessage') });
+  });
+
+  // process the login form
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/login', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
+
+  // SIGNUP =================================
+  // show the signup form
+  app.get('/signup', function (req, res) {
+    res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
+
+  // process the signup form
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect: '/profile', // redirect to the secure profile section
+    failureRedirect: '/signup', // redirect back to the signup page if there is an error
+    failureFlash: true // allow flash messages
+  }));
+
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
+
+  // local -----------------------------------
+  app.get('/unlink/local', isLoggedIn, function (req, res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function (err) {
+      res.redirect('/profile');
+    });
+  });
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated())
+    return next();
+
+  res.redirect('/');
+}
---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://sydnei:taco@cluster0.hyfpkiu.mongodb.net/sydnei-apps?retryWrites=true&w=majority', 
+    'dbName': 'sydnei-apps'
+};
